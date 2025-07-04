9fd94ee8d44098437a4926e1fbdbed6b039bb98d
= <=>
2025-07-04 18:33:45 -0500

    segundo commit

--- src/app/form-reactivo/form-reactivo.ts
+++ src/app/form-reactivo/form-reactivo.ts
@@ -1,11 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-reactivo',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './form-reactivo.html',
   styleUrl: './form-reactivo.css'
 })
 export class FormReactivo {
+
+  formulario: FormGroup;
+  listaCorreos: string[] = [];
+
+  constructor(private formBuilder: FormBuilder) {
+    this.formulario = this.formBuilder.group({
+      correo: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  enviar() {
+    if(this.formulario.valid) {
+      this.listaCorreos.push(this.formulario.value.correo);
+      this.formulario.reset();
+    }
+  }
 
 }
